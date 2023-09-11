e2b70e5dfc58d8807606352f3254e034aa7d678f
Chrislin Molly <chrislinmolly@gmail.com>
2023-09-12 00:26:56 +0530

    App Setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyBHY7K-bBHAwdBsFbV6fwFundp9rqsG-mY",
+        authDomain: "lin-1524e.firebaseapp.com",
+        projectId: "lin-1524e",
+        storageBucket: "lin-1524e.appspot.com",
+        messagingSenderId: "67135990848",
+        appId: "1:67135990848:web:049b1167f120d730e903a5",
+        measurementId: "G-Q480WQ1LDX"
+    }
+};
